06ad095061c11630d87c66b3fda6a8afa0a0c4e3
Rahulsinghcreator <138279223+Rahulsinghcreator@users.noreply.github.com>
2023-12-02 14:19:52 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27097807
-api_hash = "9fd790a9cb1f639c921d941621d2959d"
-bot_token = "6351714036:AAFFameNDrmVv9fRHuLJ7r1mL0llIzx7NTM"
-sudo_groups = -1002074844997
+api_id = 25567551
+api_hash = "45993062e6b160a7a360d648ecc8e43a"
+bot_token = "6952811569:AAEtwaOWxuaoR_lL51jFAkSSUmQORnT2hEo"
+sudo_groups = -1002120916178
